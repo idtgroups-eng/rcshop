937f91cd9c38c9cb5fc47979195d4d4b7133ea21
Pramod Kumar <idtgroups@gmail.com>
2025-12-25 10:35:49 +0530

    Auto admin runtime create

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,19 @@
+
 from django.apps import AppConfig
 
-
 class MainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        import os
+        if os.environ.get("RENDER") == "true":
+            from django.contrib.auth.models import User
+            if not User.objects.filter(username="rcadmin").exists():
+                User.objects.create_superuser(
+                    "rcadmin",
+                    "idtgroups@gmail.com",
+                    "admin123!@#"
+                )
+
+
