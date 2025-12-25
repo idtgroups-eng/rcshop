eeafce61e7df20e11b1f03c095bb9ec102637458
Pramod Kumar <idtgroups@gmail.com>
2025-12-25 12:23:01 +0530

    Premium login, register & checkout button upgrade

--- main/apps.py
+++ main/apps.py
@@ -1,19 +1,5 @@
-
 from django.apps import AppConfig
 
+
 class MainConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
-
-    def ready(self):
-        import os
-        if os.environ.get("RENDER") == "true":
-            from django.contrib.auth.models import User
-            if not User.objects.filter(username="rcadmin").exists():
-                User.objects.create_superuser(
-                    "rcadmin",
-                    "idtgroups@gmail.com",
-                    "admin123!@#"
-                )
-
-
